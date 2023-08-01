2bc05ebd79f4ed69c55610e5ee160db82734ff25
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-08-01 14:39:42 +0300

    first commit

---
+++ Full project/06-lesson/app.js
@@ -0,0 +1,26 @@
+// let myOption = 0;
+
+// let computerOption = Math.floor(Math.random() * 3 + 1);
+
+// let result = '';
+
+// function myRock() {
+//     myOption = 1;
+//     if(myOption > 2) {
+//         result = 'You won';
+//     }
+// }
+// function myPaper() {
+//     myOption = 2;
+//     console.log(myOption)
+// }
+// function myScissors() {
+//     myOption = 3;
+//     console.log(myOption)
+// }
+
+// console.log(computerOption)
+
+// document.getElementById('result').innerHTML = 'You win!';
+
+let money = 0;
